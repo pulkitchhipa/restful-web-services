7a17aa27b2054b65c4c03475dcecc5086664aab6
pulkitchhipa <pulkitchhipa01@gmail.com>
2022-12-06 12:29:29 +0530

    first Commit

---
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
+

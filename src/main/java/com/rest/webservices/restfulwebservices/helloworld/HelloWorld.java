18f38795ddfb4b4f0d2631bbf7fe9b321341312a
pulkitchhipa <pulkitchhipa01@gmail.com>
2022-12-07 06:31:16 +0530

    Bean

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
@@ -11,5 +11,11 @@ public class HelloWorld {
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
 }
 
